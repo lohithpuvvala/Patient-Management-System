a36a37f87d313c564a56296d49ff742639cef3f5
lohithpuvvala <lohithpuvvala@gmail.com>
2026-04-25 13:00:23 +0530

    Create Patient Entity in Patient Service. Add H2 DB configuration in Application.yaml

--- patient-service/src/main/java/com/pms/patient_service/PatientServiceApplication.java
+++ patient-service/src/main/java/com/pms/patient_service/PatientServiceApplication.java
@@ -1,4 +1,4 @@
-package com.pms.patientservice;
+package com.pms.patient_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PatientServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PatientServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PatientServiceApplication.class, args);
+	}
 
 }
--- patient-service/src/test/java/com/pms/patient_service/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/pms/patient_service/PatientServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pms.patientservice;
+package com.pms.patient_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PatientServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
