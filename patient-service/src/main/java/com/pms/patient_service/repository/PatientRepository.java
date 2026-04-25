6fdac9c8b0c2ca02019ba7eeace302022587a315
lohithpuvvala <lohithpuvvala@gmail.com>
2026-04-25 13:09:22 +0530

    Add PatientRepository Class in Repository Layer

---
+++ patient-service/src/main/java/com/pms/patient_service/repository/PatientRepository.java
@@ -0,0 +1,12 @@
+package com.pms.patient_service.repository;
+
+import com.pms.patient_service.module.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+
+}
